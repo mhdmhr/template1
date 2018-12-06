aeee858f4ef5503bfe7a529034c0e0daaf0d8577
mhdmhr <mhdmhrm@gmail.com>
2018-12-06 18:55:46 +0100

    third

---
+++ js/app.js
@@ -0,0 +1,19 @@
+App = {
+
+
+  init: function() {
+    return App.render();
+  },
+
+  render: function() {
+    
+  },
+
+},
+
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
